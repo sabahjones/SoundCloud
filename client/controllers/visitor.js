c62acba7c1281c8d22512a6b4c7acc0f7019dd52
sabahjones <sabahjones@gmail.com>
2017-02-27 14:24:35 -0600

    added userlogin feature

--- client/controllers/visitor.js
+++ client/controllers/visitor.js
@@ -4,4 +4,16 @@
 */
 app.controller("visitorController", function (mainFactory, $scope) {
     console.log("Created visitor Controller");
+
+mainFactory.getuser(function(response){
+  $scope.username = response
+})
+if ($scope.username){
+  $location.url('/home')
+}
+
+$scope.login = function(){
+  mainFactory.login($scope.username)
+  $location.url('/home')
+}
 });
